b98a004f2017d38c45a8a519055bfc6d3925873b
Mohamed El Hadjaoui <achraf-2060@outlook.fr>
2022-04-10 00:48:42 +0000

    pulling what I did yesterday

--- deploy.py
+++ deploy.py
@@ -1,8 +1,9 @@
-from solcx import compile_standard
+from solcx import compile_standard, install_solc
+from web3 import  Web3
+import  json
 
 with open("./SimpleStorage.sol", "r") as file:
         simple_storage_file = file.read()
-        print(simple_storage_file)
 
 compile_sol = compile_standard(
     {
@@ -17,4 +18,9 @@ compile_sol = compile_standard(
     },
     solc_version="0.8.0"
 )
-print(compile_sol)
+with open("./compiled_code.json", "w") as file:
+        json.dump(compile_sol, file)
+
+# get bytecode
+
+# bytecode = compile_sol["contracts"]
